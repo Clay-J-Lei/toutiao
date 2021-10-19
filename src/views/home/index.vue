d2ed8c2c725fa431d983f1dd7506a940423c14b4
ubuntu <ubuntu@localhost.localdomain>
2021-10-19 21:27:50 +0800

    首页构建

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="home">首页-功能未开发</div>
+  <div class="home">
+    <van-nav-bar title="标题" left-text="返回" left-arrow>
+      <template #right>
+        <van-icon name="search" size="18" />
+      </template>
+    </van-nav-bar>
+
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item>1</van-swipe-item>
+        <van-swipe-item>2</van-swipe-item>
+        <van-swipe-item>3</van-swipe-item>
+        <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+
+    <van-grid :border="false" :column-num="3">
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-1.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-2.jpg" />
+      </van-grid-item>
+      <van-grid-item>
+        <van-image src="https://img01.yzcdn.cn/vant/apple-3.jpg" />
+      </van-grid-item>
+    </van-grid>
+    <van-cell-group inset>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+  </div>
+
 </template>
 
 <script>
@@ -9,5 +39,11 @@ export default {
 </script>
 
 <style scoped>
-
+    .my-swipe .van-swipe-item {
+      color: #fff;
+      font-size: 20px;
+      line-height: 150px;
+      text-align: center;
+      background-color: #39a9ed;
+    }
 </style>
